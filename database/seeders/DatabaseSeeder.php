80c1ae05101955ef5e4a7156419ceb24eff8030b
Sunshide12 <stebanbusiness@gmail.com>
2026-02-25 02:19:56 +0100

    feat(seeders): agregar ProductSeeder con registros de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
     {
 
         $this->call([
-            CategorySeeder::class
+            CategorySeeder::class,
+            ProductSeeder::class
         ]);
     }
 }
